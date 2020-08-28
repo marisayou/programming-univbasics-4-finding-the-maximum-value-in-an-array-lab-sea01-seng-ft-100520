8c0c3965ed280d64a41c6d1bf28410c0958fa42e
Marisa You <marisa.you@gmail.com>
2020-08-28 06:53:33 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,11 @@
 def find_max_value(array)
-  # Add your solution here
+  max = array[0]
+  
+  for i in 1...array.length 
+    if array[i] > max 
+      max = array[i]
+    end
+  end
+  
+  return max
 end
